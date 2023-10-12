00758708f386eded680980aaf670488d5f629025
nayan_dwivedi <naya.d@zecdata.com>
2023-10-12 15:29:16 +0530

    add some thing

---
+++ typeConversion.js
@@ -0,0 +1,35 @@
+//<<<<<< Number Conversion >>>>>
+/*
+let score = "33";
+let score2 = "34abc";
+console.log(Number(score)); // 33 number
+console.log(Number(score2)); // NaN number
+console.log(Number(true)); // 1
+console.log(Number(false)); // 0
+*/
+
+//<<<<<< Bool Conversion >>>>>
+/*
+console.log(Boolean("nayan")); // true
+console.log(Boolean("")); // false
+console.log(Boolean(0)); // false
+console.log(Boolean(1)); // true
+console.log(Boolean(null)); // false
+console.log(Boolean(undefined)); // false
+*/
+
+//<<<<<< String Conversion >>>>>
+/*
+console.log(String(1), typeof String(1)); // 1 string
+console.log(String(true), typeof String(true)); // true string
+console.log(String(false), typeof String(false)); // false string
+console.log(String(null), typeof String(null)); // null string
+console.log(String(undefined), typeof String(undefined)); // undefined string
+console.log(String([]), typeof String([])); // [] string
+console.log(String({}), typeof String({})); // [object Object] string
+*/
+
+/*
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+*/
