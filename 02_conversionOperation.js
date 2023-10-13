076bf24df233420d807c91a59db754bea9541028
nayan_dwivedi <naya.d@zecdata.com>
2023-10-13 23:16:05 +0530

    add conversionOperation.js

---
+++ 02_conversionOperation.js
@@ -0,0 +1,32 @@
+/*
+
+console.log("1" + 2); // 12 string
+console.log("3" - 2); // 1 number
+console.log("4" * 2); // 8 number
+console.log("5" / 2); // 2.5 number
+console.log("6" % 2); // 0 number
+console.log("7" ** 2); // 49 number
+console.log("11" & 2); // 2 number *
+console.log("12" ^ 2); // 14 number
+console.log("13" | 2); // 15 number
+console.log("2" == 2); // false boolean
+console.log("2" != 2); // true boolean
+console.log("2" === 2); // false boolean
+console.log("2" !== 2); // true boolean
+
+console.log(1 + "2"); //12 string
+console.log(4 - "2"); //2 number
+console.log(4 * "2"); //8 number
+console.log(4 / "2"); //2.5 number
+console.log(4 % "2"); //0 number
+console.log(4 ** "2"); //49 number
+console.log(4 & "2"); //2 number *
+console.log(4 ^ "2"); //14 number
+
+console.log(+true); //1 number
+console.log(+""); // 0 number
+
+console.log(1 + "1" - 1); //10 number
+console.log("1" + 1 - 1); //10 number
+
+*/
